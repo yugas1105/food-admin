60b90ac3bc0381c089dbde2c01ce9c3aea5c191c
yugas1105 <152766743+yugas1105@users.noreply.github.com>
2025-05-30 10:21:33 +0530

    changes done

--- src/componants/AddDish.jsx
+++ src/componants/AddDish.jsx
@@ -1,15 +1,105 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const AddDish = () => {
+
+  const [selectedImage, setselectedImage] = useState(null)
+
+  let submitDishData = async (e) => {
+    e.preventDefault()
+    let formData = new FormData(e.target);
+    let reqFormData = Object.fromEntries(formData.entries());
+    console.log("FDATA", reqFormData);
+
+    try {
+      let result = await axios.post("http://localhost:5000/api/createfood", {
+        ...reqFormData, foodimage: selectedImage
+      }, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+      )
+      alert("Food Saved Successfully")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-        <Box>
-            <Typography variant='h5' sx={{
-                mt:10,
-                textAlign: 'center'
-            }}>AddDish</Typography>
+
+      <Box sx={{
+        height: '85vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        mt: 9
+      }}>
+
+
+        <Box
+          component={"form"}
+          onSubmit={submitDishData}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 1.5,
+            p: 3,
+            boxShadow: 3,
+            borderRadius: 2
+          }}
+        >
+          <Typography variant='h5' sx={{ textAlign: 'center' }}>Add Dish</Typography>
+
+          <TextField
+            type='text'
+            name='foodname'
+            label='Dish Name'
+            variant='outlined'
+            fullWidth />
+
+          <TextField
+            type='text'
+            name='description'
+            label='Description'
+            variant='outlined'
+            fullWidth />
+
+          <TextField
+            type='number'
+            name='price'
+            label='Price'
+            variant='outlined'
+            fullWidth />
+
+          <FormControl>
+            <FormLabel>Select Category</FormLabel>
+            <RadioGroup row name='category' defaultValue='beverage'>
+              <FormControlLabel label="Appetizer" value="appetizer" control={<Radio />} />
+              <FormControlLabel label="Beverage" value="beverage" control={<Radio />} />
+              <FormControlLabel label="Main-course" value="main-course" control={<Radio />} />
+              <FormControlLabel label="Dessert" value="dessert" control={<Radio />} />
+            </RadioGroup>
+          </FormControl>
+
+          <TextField
+            type='file'
+            onChange={(e) => setselectedImage(e.target.files[0])}
+            name="foodimage"
+            label='Dish Image'
+            variant='outlined'
+            fullWidth />
+
+          <Button type='submit' variant='contained' color='warning' sx={{
+            mt: 2
+          }}>Add Dish</Button>
         </Box>
+        
+      </Box>
+
+
     </>
   )
 }
--- src/componants/AllDishes.jsx
+++ src/componants/AllDishes.jsx
@@ -1,15 +1,136 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Stack, Typography } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const AllDishes = () => {
+  let [allDishes, setallDishes] = useState([])
+  const [selectedCategory, setselectedCategory] = useState("All");
+  const [filterDishes, setfilterDishes] = useState([])
+  const [isOpenDialog, setisOpenDialog] = useState(false)
+  const [selectedDish, setselectedDish] = useState(null)
+
+  useEffect(() => {
+    let fetchDishes = async () => {
+      try {
+        let result = await axios.get("http://localhost:5000/api/fetchfood")
+        console.log("DATA", result.data.data);
+        setallDishes(result.data.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchDishes()
+  }, [])
+
+  useEffect(() => {
+    let filterDish = allDishes.filter((dish) => dish.category == selectedCategory)
+    setfilterDishes(filterDish)
+
+    if (selectedCategory == "All") {
+      setfilterDishes(allDishes)
+    }
+  }, [selectedCategory, allDishes])
+
+
+  let openDialog = (Dish) => {
+    setselectedDish(Dish)
+    setisOpenDialog(true)
+  }
+
+  let closeDialog = () => {
+    setselectedDish(null)
+    setisOpenDialog(false)
+  }
+
   return (
     <>
-        <Box>
-            <Typography variant='h5' sx={{
-                mt:10,
-                textAlign: 'center'
-            }}>All Dishes</Typography>
-        </Box>
+      <Box sx={{ mt: 11 }}>
+
+        <Stack sx={{ justifyContent: "center" }} direction="row" spacing={2}>
+          <Chip
+            label="All"
+            onClick={() => setselectedCategory("All")}
+            color={selectedCategory === "All" ? "info" : "default"}
+            variant={selectedCategory === "All" ? "filled" : "outlined"}
+          />
+          <Chip
+            label="Appetizer"
+            onClick={() => setselectedCategory("appetizer")}
+            color={selectedCategory === "appetizer" ? "info" : "default"}
+            variant={selectedCategory === "appetizer" ? "filled" : "outlined"}
+          />
+          <Chip
+            label="Dessert"
+            onClick={() => setselectedCategory("dessert")}
+            color={selectedCategory === "dessert" ? "info" : "default"}
+            variant={selectedCategory === "dessert" ? "filled" : "outlined"}
+          />
+          <Chip
+            label="Beverage"
+            onClick={() => setselectedCategory("beverage")}
+            color={selectedCategory === "beverage" ? "info" : "default"}
+            variant={selectedCategory === "beverage" ? "filled" : "outlined"}
+          />
+          <Chip
+            label="Main-course"
+            onClick={() => setselectedCategory("main-course")}
+            color={selectedCategory === "main-course" ? "info" : "default"}
+            variant={selectedCategory === "main-course" ? "filled" : "outlined"}
+          />
+        </Stack>
+
+        <Grid container sx={{ gap: 2, mt: 2 }}>
+          {
+            filterDishes.map((dish) => {
+              return (
+                <Grid item size={{
+                  sm: 12,
+                  md: 6,
+                  lg: 3
+                }}
+                >
+                  <Card>
+                    <CardMedia component="img"
+                      image={`http://localhost:5000/${dish.image}`} />
+                    <CardContent>
+                      <Typography variant='h4'>{dish.foodname}</Typography>
+                      <Typography variant='body2'>{dish.description}</Typography>
+                      <Typography variant='subtitle1'>{dish.price}</Typography>
+                      <Typography variant='caption'>{dish.category}</Typography>
+                      {/* <Typography variant='caption'>{dish.image}</Typography> */}
+                    </CardContent>
+                    <CardActions>
+                      <Button variant='contained' color='secondary' onClick={() => openDialog(dish)}>Update</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })
+
+          }
+        </Grid>
+      </Box>
+
+      <Dialog open={isOpenDialog} onClose={closeDialog}>
+        <DialogTitle>Update Food Details</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {selectedDish?.foodname}
+          </DialogContentText>
+          <DialogContentText>
+            {selectedDish?.description}
+          </DialogContentText>
+          <DialogContentText>
+            {selectedDish?.price}
+          </DialogContentText>
+          <DialogContentText>
+            {selectedDish?.category}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='contained' color='error' onClick={() => closeDialog()}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
